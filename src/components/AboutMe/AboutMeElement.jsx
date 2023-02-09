47d272f1c764c9e0ca5904149d98218e7a8e698e
IvanTuly <ivantuly@gmail.com>
2023-02-09 21:36:36 +0100

    css improve

--- src/components/AboutMe/AboutMeElement.jsx
+++ src/components/AboutMe/AboutMeElement.jsx
@@ -1,18 +1,41 @@
 import React from "react";
 
 import styles from "./AboutMe.module.scss";
+
 const AboutMeElement = ({ header, obj }) => {
+  const skills = () => {
+    return obj.map((item, index) => (
+      <div key={index}>
+        <div className={styles.item_title}>
+          {item.title} <br />
+        </div>
+        <div className={styles.item_text}>
+          {item.text.split(",").map((val, index) => (
+            <div key={index} className={styles.listItem}>
+              {val}
+            </div>
+          ))}
+        </div>
+      </div>
+    ));
+  };
+
+  const base = () => {
+    return obj.map((item, index) => (
+      <div key={index}>
+        <div className={styles.item_title}>{item.title}</div>
+        {item.subtitle && (
+          <div className={styles.item_subtitle}>{item.subtitle}</div>
+        )}
+        <div className={styles.item_text}>{item.text}</div>
+      </div>
+    ));
+  };
+
   return (
     <div className={styles.item}>
       <div className={styles.item_header}>{header}</div>
-      {obj.map((item, index) => (
-        <div key={index}>
-          <div className={styles.item_title}>
-            {item.title} <br />
-          </div>
-          <div className={styles.item_text}>{item.text}</div>
-        </div>
-      ))}
+      {header.toLowerCase() === "skills" ? skills() : base()}
     </div>
   );
 };
